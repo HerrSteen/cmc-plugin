53836793358a12c73c61c7708734165762a20b35
Johan Steen <pastor.steen@gmail.com>
2021-12-28 15:45:51 +0100

    Make the plugin work

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const actionLine = document.createElement("div")
-actionLine.classList.add("action-line")
-document.body.prepend(actionLine)
+
+setTimeout(() => {
+  const actionLine = document.createElement("div")
+  actionLine.classList.add("action-line")
+  document.body.prepend(actionLine)
+}, 12000)
 
 let requestedAction = ""
 
+console.log("hello")
+
+console.log("plugin started")
+
 document.addEventListener("keydown", (e) => {
+  const actionLine = document.getElementsByClassName("action-line")[0]
+  if(!actionLine) return
+  console.log("key pressed")
+
   if (e.code === "ArrowLeft") {
     e.preventDefault()
     requestedAction = "sell"
@@ -24,17 +35,41 @@ document.addEventListener("keydown", (e) => {
   const instrumentName = getInstrumentNameFromKeyCode(e.code)
 
   if (requestedAction && instrumentName) {
-    // const row = getRowFromName(instrumentName)
-    // const { buyButton, sellButton } = getBuyAndSellButton(row)
+    const row = getRowFromName(instrumentName)
+    const { sellButton, buyButton } = getBuyAndSellButton(row)
 
     if (requestedAction === "buy") {
-      console.log("buy")
-      // buyButton.click()
+      console.log("buy", buyButton)
+      buyButton.click()
+
+      try {
+        setTimeout(() => document.querySelector(".feature-controller-div.aw12 .action-button.submit-button").click(), 1500)
+      } catch(e) {
+        console.log("kunde inte klicka på action-knapp", e)
+      }
+
+      try {
+        setTimeout(() => document.querySelector(".feature-controller-div.aw12 .action-button.close-button").click(), 2500)
+      } catch(e) {
+        console.log("kunde inte klicka på stäng-knapp", e)
+      }
     }
 
     if (requestedAction === "sell") {
-      console.log("sell")
-      // sellButton.click()
+      console.log("sell", sellButton)
+      sellButton.click()
+
+      try {
+        setTimeout(() => document.querySelector(".feature-controller-div.aw12 .action-button.submit-button").click(), 1500)
+      } catch(e) {
+        console.log("kunde inte klicka på action-knapp", e)
+      }
+
+      try {
+        setTimeout(() => document.querySelector(".feature-controller-div.aw12 .action-button.close-button").click(), 2500)
+      } catch(e) {
+        console.log("kunde inte klicka på stäng-knapp", e)
+      }
     }
   }
 
@@ -49,31 +84,33 @@ function getInstrumentNameFromKeyCode(code) {
       return "Sweden 30"
     case "KeyG":
       return "Germany 40"
+    case "KeyN":
+      return "US NDAQ 100"
+    case "KeyU":
+      return "US SPX 500"
     default:
     return null
   }
 }
 
-function getBuyAndSellButton(row) {
-  const columns = row.getElementsByClassName("column")
-  const columnCount = columns.length - 1
-
-  const buyButton = columns[columnCount].getElementsByClassName("double-row-actions__bid")[0]
-  const sellButton = columns[columnCount].getElementsByClassName("double-row-actions__offer")[0]
+function getBuyAndSellButton(tile) {
+  console.log("getBuyAndSellButton", tile)
+  const sellButton = tile.querySelector(".price-box.sell")
+  const buyButton = tile.querySelector(".price-box.buy")
 
   return {
-    buyButton,
     sellButton,
+    buyButton,
   }
 }
 
 function getRowFromName(name) {
-  const wrapper = document.getElementsByClassName('feature-next-gen-watchlist')[0]
-  const rows = wrapper.getElementsByClassName("row")
+  const wrapper = document.getElementsByClassName("vertical-container")[0]
+  const gridTiles = wrapper.getElementsByClassName("grid-tile")
 
-  const z = [...rows].filter((row) => {
-    const columns = row.getElementsByClassName("column")
-    return columns[0].getAttribute("title") === name
+  const z = [...gridTiles].filter((tile) => {
+    const nameTile = tile.getElementsByClassName("grid-tile__title")[0]
+    return nameTile.innerText === name
   });
 
   return z[0]
